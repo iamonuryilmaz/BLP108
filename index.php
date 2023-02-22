ac97728b96a889820e618095d682e0a1b0904814
Onur Yılmaz <muh.onuryilmaz@gmail.com>
2023-02-22 19:36:01 +0300

    genel tanıtım

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
-MERHABA
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>İlk PHP Projemiz</h1>
+
+<?php
+
+$mesaj = "Merhaba";
+$mesaj2 = 'Dünya !';
+
+
+$tamMetin = $mesaj." ".$mesaj2;
+
+//Console.Write(mesaj);
+
+//echo ekrana yazdırma fonksiyonudur.
+//echo "$mesaj $mesaj2";
+
+
+echo "<h2>". $tamMetin ."</h2>";
+
+/*
+    echo "Merhaba Dünya !...<br>";
+    echo "Merhaba Dünya !...<br>";
+    echo "Merhaba Dünya !...<br>";
+    echo "Merhaba Dünya !...<br>";
+    echo "Merhaba Dünya !...<br>";
+    echo "Merhaba Dünya !...<br>";
+    echo "Merhaba Dünya !...<br>";
+*/
+
+?>
+
+</body>
+</html>
